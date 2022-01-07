b604970d3aefab300396b9c940c2e1251a02c943
Emre Badem <emrebadem_@hotmail.com>
2022-01-07 12:08:53 +0300

    Handle empty NODE_ENV

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import dotenv from 'dotenv';
 import connectToMongo from './connections/mongodb';
 import createServer from './server';
 
-if (process.env.NODE_ENV === 'development') dotenv.config();
+if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) dotenv.config();
 
 connectToMongo(process.env.MONGODB_URL).then(createServer).catch(console.error);
